7e0c9668ea4b1b86d3167952ab005d727fb388ee
xp <xp@Android011>
2016-10-14 17:03:32 +0200

    Añadido al icono de usuario un menu dropdown

--- juego_adivina.php
+++ juego_adivina.php
@@ -1,8 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Trivial</title>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!-- Latest compiled and minified JS -->
 
+
+
+</head>
+<body>
+
+
+</body>
+</html>
--- menu_inicio.php
+++ menu_inicio.php
@@ -4,18 +4,17 @@
 //HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 
-if (!isset($_SESSION) ) {
+if (!isset($_SESSION)) {
     session_start();
 }
 $nombre = $_SESSION['Nombre'];
 $apellido = $_SESSION['Apellidos']
-
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
-        
+
         <link rel="stylesheet" href="js/jquery-3.1.0.min.js">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/prueba.css">
@@ -27,211 +26,214 @@ $apellido = $_SESSION['Apellidos']
     <body>
 
         <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Histología</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-              <li><a href="../navbar-static-top/"><?php echo $nombre;echo ' ';?><?php echo $apellido; ?></a></li>
-            <li><img src="img/<?php echo $_SESSION['DNI'];?>.jpg" 
-                   class="img-circle" style="width:60px;padding:10px;"></li>
-            <li><a href="./">Cerrar Sesión</a></li>
-            
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
-<div class="container">
-
-<?php
-// ejemplo de volcado de una query a un array en php
-//creo el array
-$usuarios = array();
-//hago la consulta a la BBDD
-$consulta_usuarios = $mysqli -> query ("select * from usuario");
-//saco el numero de usuarios que hay en la bbdd
-$num_usuarios = $consulta_usuarios -> num_rows;
-//monto un bucle for para cargar en el array los resultados de la query
-for ($i = 0; $i < $num_usuarios; $i++){
-    $r = $consulta_usuarios -> fetch_array();
-    $usuarios[$i][0]=$r['DNI'];
-    $usuarios[$i][2]=$r['Nombre'];
-    $usuarios[$i][3]=$r['Apellidos'];
-    $usuarios[$i][4]=$r['Email'];
-}
-//ahora voy a usar los datos en un ejemplo
-?>
-
-</table>
-    <br>
-    <br>
-</div>
-        <br>
-        <br>
-         <div class="row">
-        <h2 class="text-center" style="color:white;">PRACTIQUEMOS</h2>
-         </div>
-        <br>
-        <div class="container" id="centro">
             <div class="container">
-         
-                <div class="gallery">
-                    <ul>
-                        <li>
-
-                            <div class='wrapper' id="menu">
-                                <!-- image -->
-                                <img class="agran" src="img/ovejaPareja.jpg" />
-                                <!-- description div -->
-                                <div class='description' >
-                                    <!-- description content -->
-                                    <p class='description_content'>Cada oveja con su pareja</p>
-                                    <!-- end description content -->
-                                </div>
-                                <!-- end description div -->
-
-                            </div>
-
-                        </li>
-                        <li>
-
-                            <div class='wrapper' id="menu">
-                                <!-- image -->
-                                <img class="agran" src="img/ovejaPareja.jpg" />
-                                <!-- description div -->
-                                <div class='description' >
-                                    <!-- description content -->
-                                    <p class='description_content'>Cada oveja con su pareja</p>
-                                    <!-- end description content -->
-                                </div>
-                                <!-- end description div -->
-
-                            </div>
-
-                        </li>
-                        <li>
-
-                            <div class='wrapper' id="menu">
-                                <!-- image -->
-                                <img class="agran" src="img/ovejaPareja.jpg" />
-                                <!-- description div -->
-                                <div class='description' >
-                                    <!-- description content -->
-                                    <p class='description_content'>Cada oveja con su pareja</p>
-                                    <!-- end description content -->
-                                </div>
-                                <!-- end description div -->
-
-                            </div>
-
-                        </li>
-                        <li>
-
-                            <div class='wrapper' id="menu">
-                                <!-- image -->
-                                <img class="agran" src="img/ovejaPareja.jpg" />
-                                <!-- description div -->
-                                <div class='description' >
-                                    <!-- description content -->
-                                    <p class='description_content'>Cada oveja con su pareja</p>
-                                    <!-- end description content -->
-                                </div>
-                                <!-- end description div -->
-
-                            </div>
-
-                        </li>
-                        <li>
-
-                            <div class='wrapper' id="menu">
-                                <!-- image -->
-                                <img class="agran" src="img/ovejaPareja.jpg" />
-                                <!-- description div -->
-                                <div class='description' >
-                                    <!-- description content -->
-                                    <p class='description_content'>Cada oveja con su pareja</p>
-                                    <!-- end description content -->
-                                </div>
-                                <!-- end description div -->
-
-                            </div>
-
-                        </li>
-                       <li>
-
-                            <div class='wrapper' id="menu">
-                                <!-- image -->
-                                <img class="agran" src="img/ovejaPareja.jpg" />
-                                <!-- description div -->
-                                <div class='description' >
-                                    <!-- description content -->
-                                    <p class='description_content'>Cada oveja con su pareja</p>
-                                    <!-- end description content -->
-                                </div>
-                                <!-- end description div -->
-
-                            </div>
-
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Histología</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                               
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
                         </li>
                     </ul>
-                </div>
-            </div>  
+                    <ul class="nav navbar-nav navbar-right">
+                       
+                        <li class="dropdown"> 
+                            <img data-toggle="dropdown" src="img/<?php echo $_SESSION['DNI']; ?>.jpg" 
+                                 class="img-circle dropdown-toggle" style="width:60px;padding:10px;">
+                            <ul class="dropdown-menu">
+                                 <li><a href="#">Conectado como <?php echo $nombre; echo ' '; ?><?php echo $apellido; ?></a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Tú Perfil</a></li>
+                                <li><a href="#">Ayuda</a></li>
+                                <li role="separator" class="divider"></li>                                
+                                <li><a href="#">Opciones</a></li>
+                                <li><a href="./">Cerrar Sesión</a></li>
+                            </ul>
+                        </li>                      
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
 
-        </div>
-    </body>
-    <script>
-        // Create a lightbox
+        <div class="container">
 
-        var $lightbox = $("<div class='lightbox'></div>");
-        var $img = $("<img>");
-        var $caption = $("<p class='caption'></p>");
+            <?php
+// ejemplo de volcado de una query a un array en php
+//creo el array
+            $usuarios = array();
+//hago la consulta a la BBDD
+            $consulta_usuarios = $mysqli->query("select * from usuario");
+//saco el numero de usuarios que hay en la bbdd
+            $num_usuarios = $consulta_usuarios->num_rows;
+//monto un bucle for para cargar en el array los resultados de la query
+            for ($i = 0; $i < $num_usuarios; $i++) {
+                $r = $consulta_usuarios->fetch_array();
+                $usuarios[$i][0] = $r['DNI'];
+                $usuarios[$i][2] = $r['Nombre'];
+                $usuarios[$i][3] = $r['Apellidos'];
+                $usuarios[$i][4] = $r['Email'];
+            }
+//ahora voy a usar los datos en un ejemplo
+            ?>
+
+        </table>
+        <br>
+        <br>
+    </div>
+    <br>
+    <br>
+    <div class="row">
+        <h2 class="text-center" style="color:white;">PRACTIQUEMOS</h2>
+    </div>
+    <br>
+    <div class="container" id="centro">
+        <div class="container">
+
+            <div class="gallery">
+                <ul>
+                    <li>
+
+                        <div class='wrapper' id="menu">
+                            <!-- image -->
+                            <img class="agran" src="img/ovejaPareja.jpg" />
+                            <!-- description div -->
+                            <div class='description' >
+                                <!-- description content -->
+                                <p class='description_content'>Cada oveja con su pareja</p>
+                                <!-- end description content -->
+                            </div>
+                            <!-- end description div -->
+
+                        </div>
+
+                    </li>
+                    <li>
+
+                        <div class='wrapper' id="menu">
+                            <!-- image -->
+                            <img class="agran" src="img/ovejaPareja.jpg" />
+                            <!-- description div -->
+                            <div class='description' >
+                                <!-- description content -->
+                                <p class='description_content'>Cada oveja con su pareja</p>
+                                <!-- end description content -->
+                            </div>
+                            <!-- end description div -->
+
+                        </div>
+
+                    </li>
+                    <li>
+
+                        <div class='wrapper' id="menu">
+                            <!-- image -->
+                            <img class="agran" src="img/ovejaPareja.jpg" />
+                            <!-- description div -->
+                            <div class='description' >
+                                <!-- description content -->
+                                <p class='description_content'>Cada oveja con su pareja</p>
+                                <!-- end description content -->
+                            </div>
+                            <!-- end description div -->
+
+                        </div>
+
+                    </li>
+                    <li>
+
+                        <div class='wrapper' id="menu">
+                            <!-- image -->
+                            <img class="agran" src="img/ovejaPareja.jpg" />
+                            <!-- description div -->
+                            <div class='description' >
+                                <!-- description content -->
+                                <p class='description_content'>Cada oveja con su pareja</p>
+                                <!-- end description content -->
+                            </div>
+                            <!-- end description div -->
+
+                        </div>
+
+                    </li>
+                    <li>
+
+                        <div class='wrapper' id="menu">
+                            <!-- image -->
+                            <img class="agran" src="img/ovejaPareja.jpg" />
+                            <!-- description div -->
+                            <div class='description' >
+                                <!-- description content -->
+                                <p class='description_content'>Cada oveja con su pareja</p>
+                                <!-- end description content -->
+                            </div>
+                            <!-- end description div -->
+
+                        </div>
+
+                    </li>
+                    <li>
+
+                        <div class='wrapper' id="menu">
+                            <!-- image -->
+                            <img class="agran" src="img/ovejaPareja.jpg" />
+                            <!-- description div -->
+                            <div class='description' >
+                                <!-- description content -->
+                                <p class='description_content'>Cada oveja con su pareja</p>
+                                <!-- end description content -->
+                            </div>
+                            <!-- end description div -->
+
+                        </div>
+
+                    </li>
+                </ul>
+            </div>
+        </div>  
+
+    </div>
+</body>
+<script>
+    // Create a lightbox
+
 
 
 // Add image and caption to lightbox
 
-        $lightbox
-                .append($img)
-                .append($caption);
+
 
 // Add lighbox to document
 
-        $('body').append($lightbox);
 
-        $('.wrapper').hover(
-                
+
+    $('.wrapper').hover(
             function () {
-            $(this).find('div.description').addClass('hidden');
-        }, function () {
+                $(this).find('div.description').addClass('hidden');
+            }, function () {
 
-            $(this).find('div.description').removeClass('hidden');
-        });
+        $(this).find('div.description').removeClass('hidden');
+    });
 
 
-    </script>    
+</script>    
 
 </html>
