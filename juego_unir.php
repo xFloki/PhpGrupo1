9a39976e2d4c2c61ecc1885d09a42ab1c6fbbf78
xp <xp@DESKTOP-KF4UIC2>
2016-10-18 16:53:02 +0200

    Previo al desastre

--- juego_unir.php
+++ juego_unir.php
@@ -1,8 +1,42 @@
-<?php
+<html>
+    <head>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+        <link rel="stylesheet" href="js/jquery-3.1.0.min.js">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <style>
+            #theIdOfTheBox {
+                position: absolute;
+                left:100px;
+                top:100px;
+                width:300px;
+                height:300px;
+                background:blue;
+            }
+        </style>
+        
+
+    </head>
+
+    <body>
+        <div id="draggable">box</div>
+        
+        
+        
+        <script>
+
+            $("#draggable").draggable();
+
+            $('#draggable').draggable({
+                stop: function (event, ui) {
+                    ... }
+            })
+
+        </script>
+    </body>
+
+
+
+
+</html>
