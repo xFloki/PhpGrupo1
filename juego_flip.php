a516037012a3136e3d0a6878923b06d7c8d81915
xp <xp@DESKTOP-KF4UIC2>
2016-11-10 18:34:31 +0100

    Proceso de union de flip

--- juego_flip.php
+++ juego_flip.php
@@ -1,34 +1,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
     <head>
-        
         <meta charset="utf-8"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <title>Flip</title>
-        
-        
-           <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
-           
-           
-           
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
-        
-        <script rel="stylesheet"  src="js/jquery.flip.min.js"></script>
-
-        <script type="text/javascript" src="js/script.js"></script>
-        <link rel="stylesheet" type="text/css" href="css/styles.css" />
-        <link rel="stylesheet" href="css/bootstrap.min.css"/>
-        
-        
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/styleFlip.css"/> 
+        <link rel="stylesheet" href="css/bootstrap.min.css"  />
 
 
     </head>
-
-    <body>
-        <br> <h1>Voltea la imagen para aprender más!</h1><br>
-
+    <body background="img/background.png">
         <?php
         include ('./misfunciones.php');
 
@@ -42,6 +23,10 @@
 //saco el numero de usuarios que hay en la bbdd
         $num_usuarios = $consulta_usuarios->num_rows;
 
+
+        $num_usuarios_dividido = $num_usuarios / 8;
+
+
 //monto un bucle for para cargar en el array los resultados de la query
         for ($i = 0; $i < $num_usuarios; $i++) {
             $r = $consulta_usuarios->fetch_array();
@@ -49,127 +34,202 @@
             $imagenes[$i][1] = $r['Descripcion'];
             $imagenes[$i][3] = $r['Nombre_muestra'];
         }
+        $contador = 0;
 
-//ahora voy a usar los datos en un ejemplo
-//        
+//ahora voy a usar los datos en un ejemplo     
         ?>
-
-
-
-
-        <?php
-// Each sponsor is an element of the $sponsors array:
-//$imagenes=$sponsors;
-//        $sponsors = array(
-//            array('facebook', 'The biggest social network in the world.', 'http://www.facebook.com/'),
-//            array('adobe', 'The leading software developer targeted at web designers and developers.', 'http://www.adobe.com/'),
-//            array('microsoft', 'One of the top software companies of the world.', 'http://www.microsoft.com/'),
-//            array('sony', 'A global multibillion electronics and entertainment company ', 'http://www.sony.com/'),
-//            array('dell', 'One of the biggest computer developers and assemblers.', 'http://www.dell.com/'),
-//            array('ebay', 'The biggest online auction and shopping websites.', 'http://www.ebay.com/'),
-//            array('digg', 'One of the most popular web 2.0 social networks.', 'http://www.digg.com/'),
-//            array('google', 'The company that redefined web search.', 'http://www.google.com/'),
-//            array('ea', 'The biggest computer game manufacturer.', 'http://www.ea.com/'),
-//            array('mysql', 'The most popular open source database engine.', 'http://www.mysql.com/'),
-//            array('hp', 'One of the biggest computer manufacturers.', 'http://www.hp.com/'),
-//            array('yahoo', 'The most popular network of social media portals and services.', 'http://www.yahoo.com/'),
-//            array('cisco', 'The biggest networking and communications technology manufacturer.', 'http://www.cisco.com/'),
-//            array('vimeo', 'A popular video-centric social networking site.', 'http://www.vimeo.com/'),
-//            array('canon', 'Imaging and optical technology manufacturer.', 'http://www.canon.com/')
-//        );
-// Randomizing the order of sponsors:
-        ?>
-
-
-
-        <div id="main">
-
-
-
-            <div class="row-fluid">
-
-
+        <div id="main"  style="margin:5% 10% 0 10%" >
+            <div class="row  ">
                 <?php
 // Looping through the array:
-                shuffle($imagenes);
-
-                for ($i = 0; $i < 8; $i++) {
+                ?>
 
 
-                    echo'
-                                  
-        
-        <div class="col-lg-3 col-md-4 col-xs-12 col-sm-6 centered" >
-            <div class="sponsor " title="Click to flip">
+                <div id="myCarousel" class="carousel slide"  data-interval="false" >
 
 
-                <div class="sponsorFlip">
-                    <img src="' . $imagenes[$i][0] . '" alt="More about ' . $imagenes[$i][0] . '"/>
+                    <!-- Carousel items -->
+                    <div class="carousel-inner ">
+
+                        <?php
+// Looping through the array:
+//                        shuffle($imagenes);
+
+
+                        for ($j = 0; $j < floor($num_usuarios_dividido); $j++) {
+                            if ($j == 0) {
+                                $active = "active";
+                            } else {
+                                $active = "";
+                            }
+                            echo'
+        <div class="' . $active . ' item">';
+
+
+                            for ($i = 0; $i < 8; $i++) {
+                                echo'
+             <div class="col-lg-3 col-md-6 col-xs-12 col-sm-6 text" style="height:300px; margin-top:15px;  "  >
+                <div id="card-' . $i . '">
+                    <br/>
+                    <div class="front"> 
+                        <img class="img2" src="' . $imagenes[$contador][0] . '" ></img>
+                    </div> 
+
+                    <div class="back"> 
+                        <h3 style="margin:15px; color: #46b8da;">' . $imagenes[$contador][3] . '</h3>
+                      <p style="margin:18px;"> ' . $descripcion = substr($imagenes[$contador][1], 0, 170) . '
+                        <button style="color:white;"onclick="myFunction(\'' . $contador . '\')" type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal">...</button></p>
+
+                    </div> 
                 </div>
+            </div>
+            
 
-                <div class="sponsorData">
-                <div class="sponsorDescription">
-                        <h3 style="color:black;">' . $imagenes[$i][3] . '</h3>
-                    </div>
-                
-                    <div class="sponsorDescription">
-                    
-                        ' . $descripcion = substr($imagenes[$i][1], 0, 148) . '...
-                               
-                    </div>
-                    <div class="sponsorDescription">
-    
+            ';
+                                $contador = $contador + 1;
+                            }
 
-                    </div>
-          
-                    
-                      
-                
+                            echo'
+                                
+        </div>
+        
+
+
+
+                        ';
+
+
+                            echo'
+        <div class=" item">';
+
+
+                            if (($num_usuarios % 8) != 0) {
+                                for ($k = 0; $k < ($num_usuarios % 8); $k++) {
+
+                                    echo'
+             <div class="col-lg-3 col-md-6 col-xs-12 col-sm-6" style="height:300px; margin-top:15px; "  >
+                <div id="card-' . $k . '">
+                    <br/>
+                    <div class="front"> 
+                        <img class="img2" src="' . $imagenes[$contador][0] . '" ></img>
+                    </div> 
+
+                    <div class="back"> 
+                        <h4 style="align">' . $imagenes[$contador][3] . '</h4>
+                      <p> ' . $descripcion = substr($imagenes[$contador][1], 0, 200) . '
+                        <button onclick="myFunction(\'' . $contador . '\')" type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal-' . $i . '">...</button></p>
+                    </div> 
+                </div>
+            </div>
+            
+            ';
+                                    $contador = $contador + 1;
+                                }
+                                for ($k = 0; $k < (8-($num_usuarios % 8)); $k++) {
+
+                                    echo'
+             <div class="col-lg-3 col-md-6 col-xs-12 col-sm-6" style="height:300px; margin-top:15px;  "   >
+                <div id="card-' . $k . '">
+                    <br/>
                    
                 </div>
             </div>
             
-        </div>
-        
-         
-       
-         
-                         
+            ';
+                                   
+                                }
+                            }
+
+                            echo'
                                 
-				';
-                }
-                ?>
+        </div>
+                        ';
+                        }
+                        ?>
+                        
+                        <ol  class="carousel-indicators">
+                            <?php
+                            for ($l = 0; $l < ceil($num_usuarios_dividido); $l++) {
+
+                                echo '<li data-target="#myCarousel" data-slide-to="' . $l . '"></li>';
+                            }
+                            ?>
+                        </ol>
+                    </div>
 
 
 
+
+                    <div id="inferior-izquierda" href="#myCarousel" data-slide="prev">
+                       
+                        <a id="flecha" style="margin-left:20px;"  ><b>&lsaquo;</b></a>
+                        
+                 
+                    </div>
+                    <div id="inferior-derecha" href="#myCarousel" data-slide="next">
+                        <a id="flecha" style="margin-right:20px;" ><b>&rsaquo;</b></a>
+                    </div>
+
+
+
+
+                    <div class="modal fade" id="myModal" role="dialog">
+                        <div class="modal-dialog modal-lg" >
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 id="titulo_modal" class="modal-title"></h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p id="descripcion_modal"></p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button  type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+
+                </div>
             </div>
         </div>
 
-
-<!--  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Small Modal</button>
-
-   Modal 
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog modal-sm">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>This is a small modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>-->
-
-
-
-
-
     </body>
 
+    <script src="js/jquery-3.1.0.min.js"></script>
+    <script src="js/jquery.flip.minSilvia.js" ></script>
+    <script src="js/bootstrap.js" ></script>
+
+    <script>
+        $("[id^=card-]").flip({
+            axis: 'x',
+            trigger: 'click',
+            autoSize: false});
+
+    </script>
+    <script>
+        $(document).ready(function () {
+            $('.myCarousel').carousel({
+                wrap: false
+            });
+        });
+
+
+
+    </script>
+    <script>
+
+
+        function myFunction(_num) {
+        var lista=<?php echo json_encode($imagenes);?>  
+        $('#titulo_modal').html(
+                lista[_num][3]
+            );
+    $('#descripcion_modal').html(
+                lista[_num][1]
+            );
+        }
+        ;
+
+    </script>
 </html>
